41a496235bd6ab708f1f06ae046ce049cc9f73ce
leonardo.bessa <leonardo@editoraenovus.com.br>
2023-12-19 16:52:58 -0300

    Acelerador.Controller.php

--- BancoDados/Atualizacao.sql
+++ BancoDados/Atualizacao.sql
@@ -10,3 +10,9 @@ DELETE FROM atacadao10.TB_PERFIL_FUNCIONALIDADE where co_funcionalidade in (48);
 
 INSERT INTO atacadao10.TB_PERFIL_FUNCIONALIDADE (co_funcionalidade, co_perfil) VALUES ("48", "2");
 
+UPDATE atacadao10.TB_FUNCIONALIDADE SET no_funcionalidade = "Lucros", ds_action = "Lucros", st_menu = "S", co_controller = "14" where co_funcionalidade = 48;
+
+DELETE FROM atacadao10.TB_PERFIL_FUNCIONALIDADE where co_funcionalidade in (48);
+
+INSERT INTO atacadao10.TB_PERFIL_FUNCIONALIDADE (co_funcionalidade, co_perfil) VALUES ("48", "2");
+
