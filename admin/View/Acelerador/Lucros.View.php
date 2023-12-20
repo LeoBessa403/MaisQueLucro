e7693cd83b3e4f276b70857b56b856b8db863115
leonardo.bessa <leonardo@editoraenovus.com.br>
2023-12-20 16:56:53 -0300

    Acelerador.Controller.php

--- admin/View/Acelerador/Lucros.View.php
+++ admin/View/Acelerador/Lucros.View.php
@@ -1,7 +1,41 @@
 <style>
+    .pricing-table {
+        height: 320px;
+    }
+
     .pricing-table h2 {
         border-top-left-radius: 10px;
         border-top-right-radius: 10px;
+    }
+
+    .titulo {
+        font-size: 1.5em !important;
+        font-style: unset !important;
+        padding: 5px 3px;
+    }
+
+    .div_graf1 {
+        float: left;
+        height: 90%;
+        width: 30%;
+        margin-left: 15%;
+        box-shadow: 1px 1px 10px black;
+        border: 1px solid black;
+        margin-top: 4%;
+        font-size: 1.3em;
+        border-radius: 10px;
+        color: white;
+        font-weight: 600;
+    }
+
+    .div_graf1 span{
+        display: inline-block;
+        width: 100%;
+        padding: 5px;
+    }
+
+    #receita {
+        background-color: green;
     }
 </style>
 <div class="main-content">
@@ -10,13 +44,219 @@
         <div class="row">
             <div class="col-md-12">
                 <div id="pricing_table_example2" class="row">
-                    <div class="col-sm-4">
+                    <div class="col-sm-12">
                         <h3>Simulador 1</h3>
                         <div class="pricing-table col-sm-12 col-xs-12">
                             <div class="top">
-                                <h2 style="background-color: blue;">Dados Iniciais</h2>
+                                <h2 style="background-color: blue;">ACELERADOR DE LUCROS</h2>
                             </div>
-                            <h1 style="margin-top: 7%"><sup>R$</sup><span id="ponto_equilibrio"></span></h1>
+
+                            <div class="col-md-6" style="height: 100px">
+                                <div id="pricing_table_example2" class="row">
+                                    <div class="col-sm-12">
+                                        <h3>DADOS INICIAIS</h3>
+                                        <div class="pricing-table col-sm-12 col-xs-12">
+
+                                            <table class="table-striped table-bordered table-hover table-full-width"
+                                                   id="pagRecebi" style="width: 100%">
+                                                <thead>
+                                                <tr style="background-color: #4F4F4F; color: #00bcd4;">
+                                                    <th class="titulo" colspan="2">Qual Lucro Base você deseja
+                                                        avaliar?
+                                                    </th>
+                                                    <th class="titulo">10%</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Faturamento Mensal</td>
+                                                    <td class="titulo">50.000,00</td>
+                                                    <td class="titulo">100%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Custos Variáveis</td>
+                                                    <td class="titulo">30.000,00</td>
+                                                    <td class="titulo">60%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Margem de Contribuição</td>
+                                                    <td class="titulo">20.000,00</td>
+                                                    <td class="titulo">40%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Despesas Fixas e Investimentos</td>
+                                                    <td class="titulo">10.000,00</td>
+                                                    <td class="titulo">20%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo"><b>Lucro Operacional</b></td>
+                                                    <td class="titulo">10.000,00</td>
+                                                    <td class="titulo">20%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Preço Médio</td>
+                                                    <td class="titulo">50.000,00</td>
+                                                    <td class="titulo">100%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Quantidade vendida (mês)</td>
+                                                    <td class="titulo">50.000,00</td>
+                                                    <td class="titulo">100%</td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div id="pricing_table_example2" class="row">
+                                    <div class="col-sm-12">
+                                        <h3>GRÁFICO 1</h3>
+                                        <div id="graf1" class="pricing-table col-sm-12 col-xs-12">
+                                            <div id="receita" class="div_graf1">
+                                                <span>50.000,00</span>
+                                                <span>Faturamento / Receita</span>
+                                            </div>
+                                            <div id="desp" class="div_graf1">
+                                                <span style="background-color: darkred; height: 60%">Custos Variáveis</span>
+                                                <span style="background-color: #ee5f5b; height: 20%">Despesas Fixas</span>
+                                                <span style="background-color: #0a90eb; height: 20%">Lucro Operacional</span>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="col-md-6">
+                                <div id="pricing_table_example2" class="row">
+                                    <div class="col-sm-12">
+                                        <h3>DADOS INICIAIS</h3>
+                                        <div class="pricing-table col-sm-12 col-xs-12">
+
+                                            <table class="table-striped table-bordered table-hover table-full-width"
+                                                   id="pagRecebi" style="width: 100%">
+                                                <thead>
+                                                <tr style="background-color: #4F4F4F; color: #00bcd4;">
+                                                    <th class="titulo" colspan="2">Qual Lucro Base você deseja
+                                                        avaliar?
+                                                    </th>
+                                                    <th class="titulo">10%</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Faturamento Mensal</td>
+                                                    <td class="titulo">50.000,00</td>
+                                                    <td class="titulo">100%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Custos Variáveis</td>
+                                                    <td class="titulo">30.000,00</td>
+                                                    <td class="titulo">60%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Margem de Contribuição</td>
+                                                    <td class="titulo">20.000,00</td>
+                                                    <td class="titulo">40%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Despesas Fixas e Investimentos</td>
+                                                    <td class="titulo">10.000,00</td>
+                                                    <td class="titulo">20%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo"><b>Lucro Operacional</b></td>
+                                                    <td class="titulo">10.000,00</td>
+                                                    <td class="titulo">20%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Preço Médio</td>
+                                                    <td class="titulo">50.000,00</td>
+                                                    <td class="titulo">100%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Quantidade vendida (mês)</td>
+                                                    <td class="titulo">50.000,00</td>
+                                                    <td class="titulo">100%</td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="col-md-6">
+                                <div id="pricing_table_example2" class="row">
+                                    <div class="col-sm-12">
+                                        <h3>DADOS INICIAIS</h3>
+                                        <div class="pricing-table col-sm-12 col-xs-12">
+
+                                            <table class="table-striped table-bordered table-hover table-full-width"
+                                                   id="pagRecebi" style="width: 100%">
+                                                <thead>
+                                                <tr style="background-color: #4F4F4F; color: #00bcd4;">
+                                                    <th class="titulo" colspan="2">Qual Lucro Base você deseja
+                                                        avaliar?
+                                                    </th>
+                                                    <th class="titulo">10%</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Faturamento Mensal</td>
+                                                    <td class="titulo">50.000,00</td>
+                                                    <td class="titulo">100%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Custos Variáveis</td>
+                                                    <td class="titulo">30.000,00</td>
+                                                    <td class="titulo">60%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Margem de Contribuição</td>
+                                                    <td class="titulo">20.000,00</td>
+                                                    <td class="titulo">40%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Despesas Fixas e Investimentos</td>
+                                                    <td class="titulo">10.000,00</td>
+                                                    <td class="titulo">20%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo"><b>Lucro Operacional</b></td>
+                                                    <td class="titulo">10.000,00</td>
+                                                    <td class="titulo">20%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Preço Médio</td>
+                                                    <td class="titulo">50.000,00</td>
+                                                    <td class="titulo">100%</td>
+                                                </tr>
+                                                <tr class="linha-tabela odd">
+                                                    <td class="titulo">Quantidade vendida (mês)</td>
+                                                    <td class="titulo">50.000,00</td>
+                                                    <td class="titulo">100%</td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
                         </div>
                     </div>
                 </div>
