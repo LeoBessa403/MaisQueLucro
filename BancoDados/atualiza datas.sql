99868a14af9f5b595c846b5d50f5407fe7fa02d2
LeoBessa403 <leonardomcbessa@gmail.com>
2024-04-22 07:41:26 -0300

    Turbinador de Lucros

---
+++ BancoDados/atualiza datas.sql
@@ -0,0 +1,26 @@
+select f1.co_fluxo_caixa,
+       f1.dt_vencimento,
+       DATE_ADD((select dt_vencimento from tb_fluxo_caixa f2 where f1.co_fluxo_caixa = f2.co_fluxo_caixa), INTERVAL +9
+                month) as novo_vencimento,
+       f1.dt_cadastro,
+       DATE_ADD((select dt_cadastro from tb_fluxo_caixa f2 where f1.co_fluxo_caixa = f2.co_fluxo_caixa), INTERVAL +9
+                month) as novo_cadastro,
+       f1.dt_realizado,
+       DATE_ADD((select dt_realizado from tb_fluxo_caixa f2 where f1.co_fluxo_caixa = f2.co_fluxo_caixa), INTERVAL +9
+                month) as novo_dt_realizado
+from tb_fluxo_caixa f1
+where 1 = 1;
+#       dt_vencimento BETWEEN DATE_ADD(CURRENT_DATE(), INTERVAL -9 month) AND CURRENT_DATE()
+#   and
+
+update tb_fluxo_caixa f1
+set dt_vencimento = CONVERT(DATE_ADD((select dt_vencimento from tb_fluxo_caixa f2
+where f1.co_fluxo_caixa = f2.co_fluxo_caixa),
+                             INTERVAL +9 month), DATE ),
+dt_cadastro = CONVERT(DATE_ADD((select dt_cadastro from tb_fluxo_caixa f3 where f1.co_fluxo_caixa = f3.co_fluxo_caixa),
+                             INTERVAL +9 month), DATETIME ),
+dt_realizado = CONVERT(DATE_ADD((select dt_realizado from tb_fluxo_caixa f4 where f1.co_fluxo_caixa = f4.co_fluxo_caixa),
+                             INTERVAL +9 month), DATE )
+where 1 = 1;
+
+
