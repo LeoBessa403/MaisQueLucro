ac7b13d6d332cf89603c7015c967b93a7517a7cb
leonardo.bessa <leonardo@editoraenovus.com.br>
2024-05-03 09:24:16 -0300

    Melhorias Querys

--- library/Helpers/Upload.class.php
+++ library/Helpers/Upload.class.php
@@ -48,31 +48,43 @@ class Upload
     public function UploadImagens($Files, $Nome, $Folder = null, $Width = null)
     {
         $Nome_Arquivo = [];
-        for ($i = 0; $i < count($Files["name"]); $i++):
-            if (count($Files["name"]) < 2):
-                if (is_array($Files["name"])) {
-                    $Arquivo['name'] = $Files['name'][0];
-                    $Arquivo['type'] = $Files['type'][0];
-                    $Arquivo['tmp_name'] = $Files['tmp_name'][0];
-                    $Arquivo['error'] = $Files['error'][0];
-                    $Arquivo['size'] = $Files['size'][0];
-                } else {
-                    $Arquivo['name'] = $Files['name'];
-                    $Arquivo['type'] = $Files['type'];
-                    $Arquivo['tmp_name'] = $Files['tmp_name'];
-                    $Arquivo['error'] = $Files['error'];
-                    $Arquivo['size'] = $Files['size'];
-                }
-            else:
-                $Arquivo['name'] = $Files['name'][$i];
-                $Arquivo['type'] = $Files['type'][$i];
-                $Arquivo['tmp_name'] = $Files['tmp_name'][$i];
-                $Arquivo['error'] = $Files['error'][$i];
-                $Arquivo['size'] = $Files['size'][$i];
-            endif;
+        if (is_array($Files["name"])):
+            for ($i = 0; $i < count($Files["name"]); $i++):
+                if (count($Files["name"]) < 2):
+                    if (is_array($Files["name"])) {
+                        $Arquivo['name'] = $Files['name'][0];
+                        $Arquivo['type'] = $Files['type'][0];
+                        $Arquivo['tmp_name'] = $Files['tmp_name'][0];
+                        $Arquivo['error'] = $Files['error'][0];
+                        $Arquivo['size'] = $Files['size'][0];
+                    } else {
+                        $Arquivo['name'] = $Files['name'];
+                        $Arquivo['type'] = $Files['type'];
+                        $Arquivo['tmp_name'] = $Files['tmp_name'];
+                        $Arquivo['error'] = $Files['error'];
+                        $Arquivo['size'] = $Files['size'];
+                    }
+                else:
+                    $Arquivo['name'] = $Files['name'][$i];
+                    $Arquivo['type'] = $Files['type'][$i];
+                    $Arquivo['tmp_name'] = $Files['tmp_name'][$i];
+                    $Arquivo['error'] = $Files['error'][$i];
+                    $Arquivo['size'] = $Files['size'][$i];
+                endif;
+                $this->UploadImagem($Arquivo, $Nome, $Folder, $Width);
+                $Nome_Arquivo[$i] = $this->getResult();
+            endfor;
+        else:
+            $Arquivo['name'] = $Files['name'];
+            $Arquivo['type'] = $Files['type'];
+            $Arquivo['tmp_name'] = $Files['tmp_name'];
+            $Arquivo['error'] = $Files['error'];
+            $Arquivo['size'] = $Files['size'];
+
             $this->UploadImagem($Arquivo, $Nome, $Folder, $Width);
-            $Nome_Arquivo[$i] = $this->getResult();
-        endfor;
+            $Nome_Arquivo[0] = $this->getResult();
+        endif;
+
         return $Nome_Arquivo;
     }
 
@@ -193,7 +205,7 @@ class Upload
 
         $this->CreateFolder($this->Folder);
         $this->setFileName();
-        $this->UploadImage();
+        return $this->UploadImage();
     }
 
     //Verifica e cria o diretório base!
@@ -216,6 +228,13 @@ class Upload
     private function setFileName()
     {
         $FileName = Valida::ValNome($this->Name) . '-' . uniqid() . strrchr($this->File['name'], '.');
+        $this->Name = $FileName;
+    }
+
+    //Verifica e monta o nome dos arquivos tratando a string!
+    private function setFileName2()
+    {
+        $FileName = Valida::ValNome($this->Name) . '-' . uniqid() . strrchr($this->File['name'][0], '.');
         $this->Name = $FileName;
     }
 
@@ -271,6 +290,7 @@ class Upload
             imagedestroy($this->Image);
             imagedestroy($NewImage);
         endif;
+        return $this->Result;
     }
 
     //Envia arquivos e mídias
@@ -283,6 +303,59 @@ class Upload
             $this->Result = false;
             $this->Error = 'Erro ao mover o arquivo. Favor tente mais tarde!';
         endif;
+    }
+
+    //Envia arquivos e mídias
+    private function MoveFile2()
+    {
+        if (move_uploaded_file($this->File['tmp_name'][0], self::$BaseDir . $this->Folder . $this->Name)):
+            $this->Result = $this->Name;
+            $this->Error = null;
+        else:
+            $this->Result = false;
+            $this->Error = 'Erro ao mover o arquivo. Favor tente mais tarde!';
+        endif;
+    }
+
+    /**
+     * <b>Enviar Mídia:</b> Basta envelopar um $_FILES de uma mídia e caso queira um nome e um tamanho personalizado.
+     * Caso não informe o tamanho será 40mb!
+     * @param FILES $Media = Enviar envelope de $_FILES (XML)
+     * @param STRING $Name = Nome do arquivo ( ou do artigo )
+     * @param STRING $Folder = Pasta personalizada
+     * @param STRING $MaxFileSize = Tamanho máximo do arquivo (40mb)
+     */
+    public function UploadArquivoXML(array $midia, $Name = null, $Folder = null, $MaxFileSize = null)
+    {
+        $this->File = $midia;
+        $this->Name = ((string)$Name ? $Name : substr($midia['name'][0], 0, strrpos($midia['name'][0], '.')));
+        $this->Folder = ((string)$Folder ? $Folder : 'medias');
+        $MaxFileSize = ((int)$MaxFileSize ? $MaxFileSize : 40);
+
+        $FileAccept = array(
+            'text/xml',
+            'text/csv',
+        );
+
+        if ($this->File['size'][0] > ($MaxFileSize * (1024 * 1024))):
+            $this->Result = false;
+            $this->Error = "Arquivo muito grande, tamanho máximo permitido de {$MaxFileSize}mb";
+        elseif (!in_array($this->File['type'][0], $FileAccept)):
+            $this->Result = false;
+            $this->Error = 'Tipo de arquivo não suportado. Envie .XML!';
+        else:
+            $this->CreateFolder($this->Folder);
+            $this->setFileName2();
+            $this->MoveFile2();
+            $this->Result = 'Arquivo Transferido com Sucesso!';
+            $this->Error = false;
+        endif;
+
+        return [
+            $this->Result,
+            $this->Error,
+            $this->Name
+        ];
     }
 
 }
