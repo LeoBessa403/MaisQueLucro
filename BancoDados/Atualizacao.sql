98c29ab252818e85e2d2790981d6b8ee8be0abbf
LeoBessa403 <Admin101*/>
2024-06-11 10:57:31 -0300

    Modulo Assinante

---
+++ BancoDados/Atualizacao.sql
@@ -0,0 +1,10 @@
+INSERT INTO TB_SUPORTE (dt_cadastro, ds_assunto, st_tipo_assunto, co_assinante) VALUES ("2024-06-10 23:52:09", "dwdwdw", "1", "12");
+
+INSERT INTO sol.TB_IMAGEM (ds_caminho) VALUES ("suporte-1-6667bbda04ea5.PNG");
+
+INSERT INTO TB_HISTORICO_SUPORTE (co_suporte, dt_cadastro, ds_mensagem, st_lido, co_usuario, co_imagem) VALUES ("1", "2024-06-10 23:52:09", "<p>dd oidslkakdlsa d</p>", "N", "13", "5");
+
+INSERT INTO sol.TB_ENDERECO (ds_endereco) VALUES ("");
+
+UPDATE sol.TB_EMPRESA SET co_endereco = "6" where co_empresa = 14;
+
