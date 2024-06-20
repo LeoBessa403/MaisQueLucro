5a85ee96178937b8d92d3c7bb9daf6baf6abb372
leonardo.bessa <leonardo@editoraenovus.com.br>
2024-06-20 12:22:25 -0300

    Melhorias Tela Lançamento Combos de categorias

--- BancoDados/Atualizacao.sql
+++ BancoDados/Atualizacao.sql
@@ -26,3 +26,19 @@ DELETE FROM TB_CATEGORIA_FC_NETA where co_categoria_fc_neta = "98";
 
 UPDATE TB_CATEGORIA_FC_NETA SET ds_texto = "Almoço / Supermercado / Lanches 2" where co_categoria_fc_neta = 97;
 
+UPDATE sol.TB_FUNCIONALIDADE SET no_funcionalidade = "ChatGPT", ds_action = "ChatGPT", st_menu = "N", co_controller = "13" where co_funcionalidade = 46;
+
+DELETE FROM sol.TB_PERFIL_FUNCIONALIDADE where co_funcionalidade in (46);
+
+UPDATE sol.TB_FUNCIONALIDADE SET no_funcionalidade = "Notificação PagSeguro", ds_action = "NotificacaoPagSeguro", st_menu = "S", co_controller = "12" where co_funcionalidade = 45;
+
+DELETE FROM sol.TB_PERFIL_FUNCIONALIDADE where co_funcionalidade in (45);
+
+UPDATE sol.TB_FUNCIONALIDADE SET no_funcionalidade = "Cadastro Assinante", ds_action = "CadastroAssinante", st_menu = "N", co_controller = "12" where co_funcionalidade = 41;
+
+DELETE FROM sol.TB_PERFIL_FUNCIONALIDADE where co_funcionalidade in (41);
+
+UPDATE sol.TB_FUNCIONALIDADE SET no_funcionalidade = "Gráfico", ds_action = "GraficoRelatorio", st_menu = "S", co_controller = "8" where co_funcionalidade = 30;
+
+DELETE FROM sol.TB_PERFIL_FUNCIONALIDADE where co_funcionalidade in (30);
+
